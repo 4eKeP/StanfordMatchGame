8cf992b72b2668325218506d1e2249385c3245fa
Egor <egor_chek@mail.ru>
2023-03-11 22:10:46 +0400

    добавлена Model и ViewModel

--- StanfordMatchGame/ContentView.swift
+++ StanfordMatchGame/ContentView.swift
@@ -14,45 +14,18 @@ struct ContentView: View {
     var body: some View {
         VStack {
             ScrollView{
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))], spacing: 10) {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 75))], spacing: 10) {
                     ForEach(emojis[0..<emojiCount], id: \.self) {emoji in CardView(content: emoji)
                             .aspectRatio(2/3, contentMode: .fit)
                            // .padding(1.0)
                     }
                 }
-                
             }
             .foregroundColor(.red)
-            Spacer()
-            HStack{
-                remove
-                Spacer()
-                add
-            }
             .font(.largeTitle)
             .padding(.horizontal)
         }
         .padding(.horizontal)
-    }
-    
-    var remove: some View {
-        Button {
-            if emojiCount > 1 {
-                emojiCount -= 1
-            }
-        } label: {
-            Image(systemName: "minus.circle")
-        }
-    }
-    var add: some View {
-        Button {
-            if emojiCount < emojis.count{
-                emojiCount += 1
-            }
-        } label: {
-            Image(systemName: "plus.circle")
-            
-        }
     }
 }
 
---
+++ StanfordMatchGame/EmojiMemoryGame.swift
@@ -0,0 +1,29 @@
+//
+//  EmojiMemoryGame.swift
+//  StanfordMatchGame
+//
+//  Created by admin on 11.03.2023.
+//
+
+import SwiftUI
+
+/*
+func makeCardContent(index: Int)-> String{
+    return "🥲"
+}
+ */
+
+class EmojiMemoryGame {
+    static let emojis = ["🚗", "🚕", "🚙", "🚌", "🚎", "🏎️", "🚓", "🚑", "🚒", "🚐", "🛻", "🚚", "🚛", "🚜", "🛵", "🏍️", "🛺", "🚃", "✈️", "🚁", "⛵️", "🚲", "🛴", "🚘"]
+    static func createMemoryGame()-> MemoryGame<String>{
+        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+            emojis[pairIndex]
+        }
+    }
+    private var model: MemoryGame<String> = createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+}
+
