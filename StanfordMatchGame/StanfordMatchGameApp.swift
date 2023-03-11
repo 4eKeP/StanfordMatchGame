9936aeb8ffcd4d83354af1c77954b5e7ee8708fa
Egor <egor_chek@mail.ru>
2023-03-11 19:21:35 +0400

    Initial Commit

---
+++ StanfordMatchGame/StanfordMatchGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordMatchGameApp.swift
+//  StanfordMatchGame
+//
+//  Created by admin on 11.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct StanfordMatchGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
